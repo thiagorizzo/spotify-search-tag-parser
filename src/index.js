dc1974d95ed16dd7d563a96912204626a18abff0
Thiago Luiz Parrillo Rizzo <thiagorizzo@gmail.com>
2021-12-26 08:58:58 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const spotifySearchTags = {
+    ARTIST = 'artist',
+    ALBUM = 'album',
+    TRACK = 'track',
+    LABEL = 'label',
+    ISRC = 'isrc',
+    UPC = 'upc',
+    YEAR = 'year',
+    TAG = 'tag'
+}
+
+SpotifySearchTagParser.prototype = {
+
+    getSpotifySearchTagMatch: function(albumText, spotifySearchTag) {
+        // valid examples
+        // artist: edge of sanity album: purgatory afterglow
+        // artist: edge of sanity; purgatory afterglow
+        let tagsSpotifyRegex = new RegExp(`(?<tag>(${spotifySearchTag}:))(\s+?)?(?<tagValue>.*?)(?=artist:|album:|track:|label:|isrc:|upc:|year:|tag:|;|\n|$)`, "g");
+
+        let matchesFound = [];
+
+        let match = tagsSpotifyRegex.exec(albumText);
+        while (match != null) {
+            matchesFound.push({
+                text: match[0],
+                tagValue: match.groups.tagValue.trim(),
+                tag: spotifySearchTag  
+            });
+
+            match = tagsSpotifyRegex.exec(albumText);
+        }    
+        
+        return matchesFound;
+    },
+
+    getSpotifySearchTagMatches: function(albumText) {
+        const spotifySearchTagMatches = [];
+
+        for (const spotifySearchTag in SpotifySearchTag) {
+            let tagValue = Object.values(SpotifySearchTag).filter(t => t === spotifySearchTag.toLowerCase())[0];
+            let matchesFound = this.getSpotifySearchTagMatch(albumText, tagValue);
+            if (matchesFound.length > 0) {
+                spotifySearchTagMatches.push(...matchesFound);
+            }
+        }
+
+        return spotifySearchTagMatches;
+    },
+
+    removeSpotifySearchTag: function(albumText, spotifySearchTag) {
+        let matchesFound = this.getSpotifySearchTagMatch(albumText, spotifySearchTag);
+
+        let result = '';
+
+        matchesFound.forEach(m => {
+            albumText = albumText.replace(m.text, m.tagValue + ' ');
+        });
+        
+        return result.trim();
+    },
+    
+    removeSpotifySearchTags: function(albumText, onlyKeepTags = undefined) {
+        let matchesFound = this.getSpotifySearchTagMatches(albumText);
+
+        matchesFound
+            .forEach(m => {
+                if (onlyKeepTags === undefined || onlyKeepTags.find(x => x == m.tag) !== undefined) {
+                    albumText = albumText.replace(m.text, m.tagValue + ' ');
+                } else {
+                    albumText = albumText.replace(m.text, '');    
+                }
+            });
+        
+        return albumText.trim();
+    }
+}
+
+module.exports = SpotifySearchTagParser;
